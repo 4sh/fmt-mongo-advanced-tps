a4138d8f33d5f25e91fc38a57b593f00e49d2e03
Élise Rubio <elise.rubio@4sh.fr>
2024-02-05 10:28:40 +0100

    Requests for tp 1 step 1 solution

--- tp1.js
+++ tp1.js
@@ -1,3 +1,15 @@
 print(db.products.find({
-    // Write your request here
-}))
+    description: {$regex: /fleur.*ouvrage/, $options: 's'}
+}));
+
+print(db.products.find({
+    description: /^Les/
+}));
+
+print(db.products.find({
+    name: /image/i
+}));
+
+print(db.products.find({
+    description: {$regex: /livre.*fleur/, $options: 'is'}
+}));
