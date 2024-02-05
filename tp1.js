ba65cfdb70a58a3bc258683a9be57a307cb2bf35
Élise Rubio <elise.rubio@4sh.fr>
2024-02-05 10:29:41 +0100

    Remove unnecessary code from step 1

--- tp1.js
+++ tp1.js
@@ -1,15 +1,3 @@
 print(db.products.find({
-    description: {$regex: /fleur.*ouvrage/, $options: 's'}
-}));
 
-print(db.products.find({
-    description: /^Les/
-}));
-
-print(db.products.find({
-    name: /image/i
-}));
-
-print(db.products.find({
-    description: {$regex: /livre.*fleur/, $options: 'is'}
 }));
