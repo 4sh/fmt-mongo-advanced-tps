2b265c700de979dc29fe287e1db39ec31aee7196
driccio <damien.riccio@4sh.fr>
2024-02-29 14:42:11 +0100

    refacto: tp1 step1 solution

--- tp1.js
+++ tp1.js
@@ -1,15 +1,20 @@
-print(db.products.find({
+// Tous les produits dont la description contient le mot “fleur” puis le mot “ouvrage” (2 documents attendus)
+db.products.find({
     description: {$regex: /fleur.*ouvrage/, $options: 's'}
-}));
+})
 
-print(db.products.find({
-    description: /^Les/
-}));
-
-print(db.products.find({
+// Tous les produits dont le nom contient “image”, quelle que soit la casse (12 documents attendus)
+db.products.find({
     name: /image/i
-}));
+})
 
-print(db.products.find({
+// Tous les produits dont la description contient le mot “livre” puis le mot “fleur”, quelle que soit la casse (6 documents attendus)
+db.products.find({
     description: {$regex: /livre.*fleur/, $options: 'is'}
-}));
+})
+
+// Bonus
+// Tous les produits dont une des lignes de la description commence par “Les” (2 documents attendus)
+db.products.find({
+    description: /^Les/m
+})
