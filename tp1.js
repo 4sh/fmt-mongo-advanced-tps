bf96fcfa75c7614c88e179011c8743e635262aea
driccio <damien.riccio@4sh.fr>
2024-02-29 14:43:28 +0100

    refacto: enrich tp1 step1

--- tp1.js
+++ tp1.js
@@ -1,3 +1,20 @@
-print(db.products.find({
-    // Write your request here
-}))
+// Tous les produits dont la description contient le mot “fleur” puis le mot “ouvrage” (2 documents attendus)
+db.products.find({
+    description: ""
+})
+
+// Tous les produits dont le nom contient “image”, quelle que soit la casse (12 documents attendus)
+db.products.find({
+    name: ""
+})
+
+// Tous les produits dont la description contient le mot “livre” puis le mot “fleur”, quelle que soit la casse (6 documents attendus)
+db.products.find({
+    description: ""
+})
+
+// Bonus
+// Tous les produits dont une des lignes de la description commence par “Les” (2 documents attendus)
+db.products.find({
+    description: ""
+})
