9dafc3e80a89402e770939e156cffe8743e9f1ac
driccio <damien.riccio@4sh.fr>
2024-02-29 15:20:27 +0100

    fix: useless print

--- tp1.js
+++ tp1.js
@@ -32,9 +32,9 @@ db.baskets.find({
 // Bonus
 // Écrire une requête pour trouver tous les paniers qui contiennent au moins un livre
 // dont le titre contient “fleur”, quelle que soit la casse, en 5 exemplaires
-print(db.baskets.find({
+db.baskets.find({
     'products': {$elemMatch: {
       'product.name': /fleur/i,
       quantity: 5
     }}
-}))
+})
