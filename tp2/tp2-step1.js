1696e258888e0b80a1f686f06486071bad10eabf
driccio <damien.riccio@4sh.fr>
2024-03-01 16:09:54 +0100

    feat: add tp2 templates

---
+++ tp2/tp2-step1.js
@@ -0,0 +1,14 @@
+// Écrire une requête d’agrégation, avec $bucket, permettant de connaître, pour chaque trimestre de l’année 2023 :
+// - Le chiffre d'affaires
+// - Le montant moyen d’une facture
+// - Le nombre de produits vendus
+db.getCollection("invoices").aggregate([
+
+])
+
+
+// Bonus
+// Écrire la même requête, mais sans $bucket
+db.getCollection("invoices").aggregate([
+
+])
---
+++ tp2/tp2-step2.js
@@ -0,0 +1,21 @@
+// Les factures portent une notion de droit de visibilité via les champs _right dans le document et certains sous-objets.
+// Écrire une requête permettant de prendre en compte cette notion de droit, grâce à $redact,
+// en respectant les règles (exemple concret sur le slide suivant) :
+// - si le champ _right est absent ou null alors le document auquel il appartient est visible de tous,
+//   mais il faut s’assurer que les sous-objets sont visibles également
+// - si le champ supérieur ou égal à 0, alors seul quelqu’un ayant un droit supérieur ou égal pourra
+//   voir l’objet dans lequel est présent ce droit.
+var currentUserRight = 0;
+
+db.getCollection("invoices").aggregate([
+
+])
+
+
+// Bonus
+// Enrichir la requête précédente, afin de ne voir que les 2 lignes de facturation le plus important de la facture.
+currentUserRight = 2;
+
+db.getCollection("invoices").aggregate([
+
+])
