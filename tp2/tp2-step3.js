ee83c9e13317ab11d72f7a0cbf5f15e0b175a595
driccio <damien.riccio@4sh.fr>
2024-03-01 16:14:11 +0100

    feat: tp2 step 3 template

---
+++ tp2/tp2-step3.js
@@ -0,0 +1,14 @@
+// Écrire une requête qui permet de faire la jointure entre le magasin “Magasin de Paris” et son inventaire.
+db.getCollection("stores").aggregate([
+
+])
+
+
+// Stocker le résultat de la requête précédente dans une nouvelle collection (nommage au choix)
+db.getCollection("stores").aggregate([
+
+])
+
+
+// Requêter dans cette nouvelle collection. La mienne s'appelle storesWithInventories.
+db.getCollection("storesWithInventories").find({})
