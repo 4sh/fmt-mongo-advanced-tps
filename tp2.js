83a50ed7f8070c079717367b836d4b6e919e44b9
driccio <damien.riccio@4sh.fr>
2024-03-01 15:51:06 +0100

    feat: tp2 step3 solution

--- tp2.js
+++ tp2.js
@@ -1,55 +1,33 @@
-// Les factures portent une notion de droit de visibilité via les champs _right dans le document et certains sous-objets.
-// Écrire une requête permettant de prendre en compte cette notion de droit, grâce à $redact,
-// en respectant les règles (exemple concret sur le slide suivant) :
-// - si le champ _right est absent ou null alors le document auquel il appartient est visible de tous,
-//   mais il faut s’assurer que les sous-objets sont visibles également
-// - si le champ supérieur ou égal à 0, alors seul quelqu’un ayant un droit supérieur ou égal pourra
-//   voir l’objet dans lequel est présent ce droit.
-var currentUserRight = 0;
-
-db.getCollection("invoices").aggregate([
+// Écrire une requête qui permet de faire la jointure entre le magasin “Magasin de Paris” et son inventaire.
+db.getCollection("stores").aggregate([
+    {$match: {name: "Magasin de Paris"}},
     {
-        $redact: {
-            $cond: {
-                if: {$gte: [currentUserRight , "$_right" ]},
-                then: "$$DESCEND",
-                else: "$$PRUNE"
-            }
+        $lookup: {
+            from: "inventories",
+            localField: "_id",
+            foreignField: "storeRef",
+            as: "inventories"
         }
     }
 ])
 
 
-// Bonus
-// Enrichir la requête précédente, afin de ne voir que les 2 lignes de facturation le plus important de la facture.
-currentUserRight = 2;
-
-db.getCollection("invoices").aggregate([
+// Stocker le résultat de la requête précédente dans une nouvelle collection (nommage au choix)
+db.getCollection("stores").aggregate([
     {
-        $redact: {
-            $cond: {
-                if: {$gte: [currentUserRight , "$_right" ]},
-                then: "$$DESCEND",
-                else: "$$PRUNE"
-            }
+        $lookup: {
+            from: "inventories",
+            localField: "_id",
+            foreignField: "storeRef",
+            pipeline: [
+                {$project: {_id: 0, productRef: "$product.eanNumber", quantity: 1}}
+            ],
+            as: "inventories",
         }
     },
-    // Surcharge du champ lines existant
-    {
-        $addFields: {
-            lines: {
-                // N'étant pas dans une opération de groupement, on ne peut pas utiliser $topN
-                // $firstN sur le tableau trié permet de s'en sortir
-                $firstN: {
-                    n: 2,
-                    input: {
-                        $sortArray: {
-                            input: "$lines",
-                            sortBy: {totalPrice: -1}
-                        }
-                    }
-                }
-            }
-        }
-    }
+    {$out: "storesWithInventories"}
 ])
+
+
+// Requêter dans cette nouvelle collection.
+db.getCollection("storesWithInventories").find({})
