be7b4d9309e5e19d54a2bb0a6a4364939aa3d431
driccio <damien.riccio@4sh.fr>
2024-03-05 15:16:56 +0100

    feat: tp3 step 2 template

---
+++ tp3/tp3-step2.js
@@ -0,0 +1,66 @@
+// Récupération aléatoire d'un panier
+const basket = db.getCollection("baskets").aggregate([{ $sample: { size: 1 } }]).toArray()[0];
+
+// Affichage du panier avant sa mise à jour.
+print(basket);
+
+// Mettre à jour le panier en augmentant tous les prix de 10%.
+db.baskets.updateOne(
+    {
+        _id: basket._id
+    },
+    {
+       // TODO
+    }
+)
+
+// Affichage du panier après sa mise à jour.
+print("After update");
+print(db.getCollection("baskets").findOne({_id: basket._id}));
+
+
+
+// Sur un autre panier, contenant des livres et des légumes, diminuer le prix des livres de 10%, en utilisant l’opérateur $.
+// Note : le prix total du panier ne pourra pas être mis à jour en conséquence !
+const basketWithBookAndVegetable = db.getCollection("baskets").aggregate([
+    {$match: {$and: [{"products.product.type": "BOOK"}, {"products.product.type": "VEGETABLE"}]}},
+    {$sample: {size: 1}}
+]).toArray()[0];
+
+// Affichage du panier avant sa mise à jour.
+print(basketWithBookAndVegetable);
+
+db.baskets.updateOne(
+    {
+        _id: basketWithBookAndVegetable._id
+    },
+    {
+        // TODO
+    }
+)
+
+// Affichage du panier après sa mise à jour.
+print("After update");
+print(db.getCollection("baskets").findOne({_id: basketWithBookAndVegetable._id}));
+
+
+
+// Bonus :
+// Écrire la requête précédente en utilisant l’opérateur $[<id>]
+// Note : le prix total du panier ne pourra pas être mis à jour en conséquence !
+
+// Affichage du panier avant sa mise à jour.
+print(basketWithBookAndVegetable);
+
+db.baskets.updateOne(
+    {
+        _id: basketWithBookAndVegetable._id
+    },
+    {
+        // TODO
+    }
+)
+
+// Affichage du panier après sa mise à jour.
+print("After update");
+print(db.getCollection("baskets").findOne({_id: basketWithBookAndVegetable._id}));
