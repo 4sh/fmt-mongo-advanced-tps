d9c6187480fbfffc245f5646460dc598d2681053
driccio <damien.riccio@4sh.fr>
2024-03-08 15:46:07 +0100

    feat: tp4 step 1 solution

--- tp4/tp4-step1.js
+++ tp4/tp4-step1.js
@@ -1,8 +1,8 @@
 const store = db.stores.aggregate([{$sample: {size: 1}}]).toArray()[0];
 
-// La requête suivante est trop lente. L'executer pour mesurer son temps d'execution.
+// La requête suivante est jugée trop lente. L'executer pour mesurer son temps d'execution.
 db.getCollection("invoices")
-    .find({"lines.product.name": /^Le.*/, "store._id": ObjectId("65e1acb9353339330f148376")})
+    .find({"lines.product.name": /^Le.*/, "store._id": store._id})
     .sort({"lines.product.name": 1})
 
 // Créer l'index qui permet d'améliorer la requête précédente et vérifier que l'execution de cette dernière est plus rapide.
