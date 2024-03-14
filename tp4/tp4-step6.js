dbff7b80b38080b530ff946501269d47922077a3
driccio <damien.riccio@4sh.fr>
2024-03-14 09:17:33 +0100

    feat: tp5 step2 solution

--- tp4/tp4-step6.js
+++
@@ -1,8 +0,0 @@
-// Bonus
-//
-// La requête suivante est lente, malgré l'index qui l'accompagne.
-db.invoices.createIndex({"customer.name": 1})
-db.invoices.find({}).sort({"customer.name": 1}).collation({locale: "fr"})
-
-// Que faire pour améliorer les choses ?
-db.invoices.createIndex({"customer.name": 1}, {name: "nameWithFrCollation", collation: {locale: "fr"}})
