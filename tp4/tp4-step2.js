378a726b66c59f99c0e8dc58e82459a8749bb450
driccio <damien.riccio@4sh.fr>
2024-03-14 10:34:13 +0100

    feat: tp4 step2 solution

--- tp4/tp4-step2.js
+++ tp4/tp4-step2.js
@@ -7,4 +7,7 @@ db.products.createIndex({type: 1, name: 1})
 // Imaginons que cette requête soit systématiquement lancée avec le type "CLOTHES".
 // Quelle optimisation pourrait être apportée ?
 
-// TODO
+db.products.createIndex({type: 1, name: 1}, {name: 'type_1_name_1_on_CLOTHES', partialFilterExpression: {type: 'CLOTHES'}})
+
+// Regardons la taille des index
+db.products.stats().indexSizes
