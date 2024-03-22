53b2640c0a566bf7243666576a66619900a96b74
driccio <damien.riccio@4sh.fr>
2024-03-22 10:42:04 +0100

    fix: tp4 solution

--- tp1/tp1-step4.js
+++ tp1/tp1-step4.js
@@ -1,11 +1,12 @@
-// Récupérer les paniers pour contenant le produit 0757100810837 et ne conserver que ces lignes en résultat.
-db.getCollection("baskets").find({"products.product._id": "0757100810837"}, {"products.$": 1})
+// Récupérer les paniers pour contenant le produit "Les fleurs du mal" et ne conserver qu'une ligne en résultat.
+db.getCollection("baskets").find({"products.product.name": "Les fleurs du mal"}, {"products.$": 1})
 
 
-// Pour tous les paniers, récupérer uniquement les lignes concernant les produits de type CLOTHES.
-db.getCollection("baskets").find({}, {"products": {$elemMatch: {type: "CLOTHES"}}})
+// Pour tous les paniers, récupérer uniquement 1 ligne concernant un produit de type CLOTHES.
+db.getCollection("baskets").find({}, {"products": {$elemMatch: {"product.type": "CLOTHES"}}})
 
 
 // Pour chaque produit, projeter son nom et un nouveau champ isExpensive indiquant si le prix unitaire
 // du produit dépasse 10.
-db.getCollection("products").find({}, {isExpensive: {$gt: ["$unitPrice", 10]}, unitPrice: 1})
+db.getCollection("products").find({}, {name: 1, isExpensive: {$gt: ["$unitPrice", 10]}})
+
