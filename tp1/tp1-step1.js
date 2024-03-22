f99dbf37f3ba4bb2b59e3f3416ea0c63a0c6a797
driccio <damien.riccio@4sh.fr>
2024-03-22 11:01:44 +0100

    fix: tp1 solution

--- tp1/tp1-step1.js
+++ tp1/tp1-step1.js
@@ -3,12 +3,12 @@ db.products.find({
     description: {$regex: /fleur.*ouvrage/, $options: 's'}
 })
 
-// Tous les produits dont le nom contient “image”, quelle que soit la casse (12 documents attendus)
+// Tous les produits dont le nom contient “image”, quelle que soit la casse (6 documents attendus)
 db.products.find({
     name: /image/i
 })
 
-// Tous les produits dont la description contient le mot “livre” puis le mot “fleur”, quelle que soit la casse (6 documents attendus)
+// Tous les produits dont la description contient le mot “livre” puis le mot “fleur”, quelle que soit la casse (3 documents attendus)
 db.products.find({
     description: {$regex: /livre.*fleur/, $options: 'is'}
 })
