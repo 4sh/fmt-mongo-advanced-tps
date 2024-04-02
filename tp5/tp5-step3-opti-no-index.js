83866d0be6df2cbba9862ae4d8c6589101130925
driccio <damien.riccio@4sh.fr>
2024-04-02 08:46:09 +0200

    fix: tp5 step 3

--- tp5/tp5-step3-opti-no-index.js
+++ tp5/tp5-step3-opti-no-index.js
@@ -1,29 +1,55 @@
-// Les index nécessaires pour nos 3 requêtes
+// Puisqu'on ne récupère qu'un seul résultat, autant transformer en findOne.
+// La recherche par regex était une recherche exacte, donc autant supprimer la regex, car les regex
+// coûtent cher et peuvent engendrer des limitations à l'usage d'index.
+// Ajout d'un projection puisque nous ne nous servons que de quelques champs dans la suite du code.
+const customer = db.customers.findOne({lastName: "BLANCHARD"}, {lastName: 1, firstName: 1});
 
-// On peut créer un index couvrant pour la recherche des clients
-// Dans ce cas, firstName et _id ne sont présents que pour satisfaire la projection. L'index est alors plus lourd, mais
-// permet d'éviter un FETCH.
-db.customers.createIndex({lastName: 1, firstName: 1, _id: 1});
+// On récupère toutes les factures du client simplement pour calculer :
+// - le nombre de factures
+// - le montant total des factures
+// - le nombre de produits achetés, par type de produit
+//
+// Plutôt que de récupérer toutes les factures et faire les calculs côté applicatif,
+// nous pouvons faire une agrégation. L'avantage est d'éviter :
+// - du coût réseau pour transporter plein de données
+// - de monter en RAM (côté applicatif) tout un tas de données inutiles
+const invoiceReport = db.invoices.aggregate([
+    {$match: {"customer._id": customer._id}},
+    {$project: {_id: 0, "customer._id": 1, totalPrice: 1, lines: 1}},
+    {$facet: {
+            total: [
+                {$group: {_id: null, nbInvoices: {$sum: 1}, totalAmountInvoiced: {$sum: "$totalPrice"}}},
+                {$project: {_id: 0, nbInvoices: 1, totalAmountInvoiced: 1}}
+            ],
+            byType: [
+                {$unwind: "$lines"},
+                {$group: {_id: "$lines.product.type", total: {$sum: "$lines.quantity"}}},
+                // On projette k et v pour pouvoir faire un $arrayToObject ensuite
+                {$project: {_id: 0, k: "$_id", v: "$total"}}
+            ]
+        }},
+    {$project: {total: {$first: "$total"}, byType: {$arrayToObject: "$byType"}}}
+]).toArray()[0];
 
-// Pour la recherche des factures
-db.invoices.createIndex({"customer._id": 1});
-// Pourquoi ne pas mettre totalPrice et lines ?
-// lines étant un tableau, Mongo ne pourra pas utiliser l'index pour donner les lignes d'un document
-// (rappel : Mongo va créer autant d'entrées dans l'index que de lignes)
-// Donc Mongo va de toute façon devoir aller chercher le document complet sur disque (FETCH).
-// C'est aussi la raison pour laquelle il n'est pas nécessaire de mettre totalPrice pour satisfaire la projection
-// puisque de toute façon un FETCH va être réalisé. Pas la peine d'alourdir l'index pour rien.
+const nbInvoices = invoiceReport.total.nbInvoices;
+const totalAmountInvoiced = invoiceReport.total.totalAmountInvoiced;
+const nbProductsBoughtByType = invoiceReport.byType;
+
+// Pas besoin de récupèrer le panier le plus récent au complet. Seul le champs totalPrice nous intéresse.
+const basket = db.baskets.find({"customer._id": customer._id}).sort({creationDate: -1}).limit(1).toArray()[0];
+let totalAmountToInvoice = 0;
+
+if (basket) {
+    totalAmountToInvoice = basket.totalPrice;
+}
 
 
-// Pour la recherche du panier
-// On peut là aussi faire un index couvrant la requête pour éviter un FETCH.
-// Selon le principe ESR, on privilégie
-db.baskets.createIndex({"customer._id": 1, creationDate: -1, totalPrice: 1});
+print("********** Summary **********")
+print("Identity : " + customer.firstName + ' ' + customer.lastName.toUpperCase())
+print("Nb of invoices : " + nbInvoices)
+print("Total amount invoiced : " + totalAmountInvoiced)
+print("Total amount to invoice : " + totalAmountToInvoice)
 
-
-// Attention !!!!!
-// Il s'agit d'un exercice basique, où le raisonnement que nous opérons se fait sur une petite portion de code.
-// La méthode qui a été appliquée, à savoir créer les index permettant de couvrir les requêtes, ne peut pas toujours
-// être appliquée. Il faut garder en tête, que la création d'un index n'est pas sans conséquence et qu'on est
-// limité à 64 par collection.
-
+for(var type in nbProductsBoughtByType) {
+    print("Total products of type " + type + " bought : " + nbProductsBoughtByType[type])
+}
