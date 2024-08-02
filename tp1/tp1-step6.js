2984f6c8fd5c3f677edcc5e48e3235fd228617d8
driccio <damien.riccio@4sh.fr>
2024-08-02 10:01:10 +0200

    feat: tp1-step6.js

---
+++ tp1/tp1-step6.js
@@ -0,0 +1,14 @@
+// Cette partie est un bonus, permettant à ceux qui ont de l'avance de pratiquer.
+// Il est possible que vous ayez besoin de choses que nous n'avons pas encore vu ou que vous ne connaissez pas,
+// auquel cas il ne faut pas hésiter à consulter la documentation de Mongo.
+
+
+// Écrire une requête permettant de récupérer uniquement les paniers contenant au moins 10 produits.
+
+
+// Écrire une requête qui, pour chaque panier contenant au moins 1 légume, le retourne avec seulement
+// les lignes concernant les légumes.
+
+
+// Écrire une requête permettant d’indiquer s’il existe une facture comportant exactement 3 lignes,
+// dont une concernant exactement 74 vêtements pour un prix d’au moins 2000€.
