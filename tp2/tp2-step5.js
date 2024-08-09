95203e332bc2165bdb8095cf70cf08e9a6bb2def
driccio <damien.riccio@4sh.fr>
2024-08-09 08:15:33 +0200

    fix: tp2 step5 summary

---
+++ tp2/tp2-step5.js
@@ -0,0 +1,9 @@
+// Calculer l’état des stocks du magasin 1 si les paniers étaient validés par les clients.
+
+
+
+// Faire une requête de mise à jour qui ajoute un champ stats sur les factures. Ce champ stats doit calculer le prix total par type de produit.
+// - Dans un premier temps, on s’autorisera de connaître exhaustivement les différents types de produit.
+// - Ensuite, on cherchera à avoir le moins de référence en dur aux types (idéalement 1).
+// - Et pour finir, on veut une solution sans aucune référence aux types. On considère donc qu’on ne connaît pas l’exhaustivité des types possibles.
+
